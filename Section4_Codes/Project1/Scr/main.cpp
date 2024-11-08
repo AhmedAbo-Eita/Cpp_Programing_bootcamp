e68f89ddf09c9d920116d9ddf60b317e32438f2e
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2024-11-08 15:11:04 +0200

    add project 1

---
+++ Section4_Codes/Project1/Scr/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    // Variable to store the input number
+    int favorit_number;
+
+    // Take input from user
+    std::cout << "Enter your favorit number between 1 and 100 = " ;
+    std::cin >> favorit_number;
+
+    // Write message to user
+    std::cout << "Amazing, That is my favorit number too " << std::endl;
+
+    return 0;
+
+}
