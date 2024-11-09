04ad6557558b0f221ce79b2b5efc4c016c7a230b
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2024-11-09 11:39:53 +0200

    edit repo arrangments

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section4_Codes/Project1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    // Variable to store the input number
+    int favorit_number;
+
+    // Take input from user
+    cout << "Enter your favorit number between 1 and 100 = " ;
+    cin >> favorit_number;
+
+    // Write message to user
+    cout << "Amazing, That is my favorit number too " << endl;
+
+    return 0;
+
+}
