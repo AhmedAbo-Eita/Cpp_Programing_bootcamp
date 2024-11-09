148758e8334dd036626f51ad2911e4aac1f7b1d6
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2024-11-09 13:07:00 +0200

    edit repo

--- Beginning_Cpp_Programming_Course/Project1/main.cpp
+++ Beginning_Cpp_Programming_Course/Project1/main.cpp
@@ -6,7 +6,7 @@ int main(){
     int favorit_number;
 
     // Take input from user
-    cout << "Enter your favorit number between 1 and 100 = " ;
+    cout << "Enter your favorit number between 1 and 100 =  ";
     cin >> favorit_number;
 
     // Write message to user
