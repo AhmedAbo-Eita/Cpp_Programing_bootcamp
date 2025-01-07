69cdeef03a82b49cbf326202c7b9f20573cc4e62
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2025-01-07 19:46:31 +0200

    add find_max_element function to determine the maximum value in an array using pointers and update README with exercise details and specifications

---
+++ Beginning_Cpp_Programming_Course/Code37/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+/**
+ * @brief      Finds the maximum element in an array of integers.
+ *
+ * @details    This function loops through an array of integers and returns the
+ *             maximum element in the array.
+ *
+ * @param[in]  arr   The array of integers to search.
+ * @param[in]  size  The number of elements in the array.
+ *
+ * @return     The maximum element in the array.
+ */
+int find_max_element(int* arr, int size)
+{
+    int counter {0};
+    int max = arr[0];
+    for(counter = 1 ; counter < size ; counter ++)
+    {
+        if (max < arr[counter])
+        {
+            max = arr[counter];
+        }
+
+    }
+    return max;
+}
+int main()
+{
+    int arr[] = {12, 45, 67, 23, 9};
+    int size  = sizeof(arr)/sizeof(arr[0]);
+
+    cout<< "the max value in array = " << find_max_element(arr,size);
+
+    return 0;
+}
