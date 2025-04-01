7202ad6aa550a7307986164b56f3323694a8e354
AhmedAbo-Eita <ahmedmasre2233@gmail.com>
2025-04-01 22:02:22 +0200

    Correct rectangle name

--- Qt_Learning_Bootcamp/Cpp_Projects/1-Class_test/main.cpp
+++ Qt_Learning_Bootcamp/Cpp_Projects/1-Class_test/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-class Regtangle{
+class Rectangle{
     //methods of class
 public:
     void set_width(int width);
@@ -11,27 +11,27 @@ private:
     double m_width {};
     double m_hight {};
 };
- // Regtangle Class implementation
-void Regtangle::set_width(int width)
+ // Rectangle Class implementation
+void Rectangle::set_width(int width)
 {
     m_width = width;
 }
-void Regtangle::set_hight(int hight)
+void Rectangle::set_hight(int hight)
 {
     m_hight = hight;
 }
-double Regtangle::get_area()const
+double Rectangle::get_area()const
 {
     return m_width*m_hight;
 }
 
 int main()
 {
-    Regtangle r;
+    Rectangle r;
     r.set_hight(5.0);
     r.set_width(10.0);
 
-    std::cout << "The area of regtanglr = " << r.get_area()<<std::endl;
+    std::cout << "The area of rectanglr = " << r.get_area()<<std::endl;
 
     return 0;
 }
