5cf7cde27d98a04ca8240ffb752dd27bdcff2e5b
Ahmed Aboeita <ahmedmasre2233@gmail.com>
2025-04-12 23:48:37 +0200

    Add connect (Signals and slots) Example

---
+++ Qt_Learning_Bootcamp/Qt_Projects/6_DiffererntConnectionSyntaxies/widget.cpp
@@ -0,0 +1,51 @@
+#include "widget.h"
+#include "./ui_widget.h"
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    //when the push botton pressed the text in lable cahnges
+
+    //string notation
+    //connect(ui->MyButton,SIGNAL(clicked()),this,SLOT(change_text()));
+
+    //Functor notation : Regular slot
+    //connect(ui->MyButton,&QPushButton::clicked, this , &Widget::change_text);
+
+    //Functor notation : Lambdas
+    connect(ui->MyButton,&QPushButton::clicked,[=](){
+        ui->label->setText("Some other text from lambda");
+
+    });
+
+
+
+    // Connect slider with progress bar
+
+    //string notation
+    connect(ui->MySlider,SIGNAL(valueChanged(int)),ui->MyProgressBar,SLOT(setValue(int)));
+    connect(ui->MySlider,SIGNAL(valueChanged(int)),this,SLOT(respond(int)));
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::respond(int value)
+{
+    qDebug() << "Value is " << value;
+}
+
+// void Widget::change_text()
+// {
+//     ui->label->setText("Some other text");
+// }
+
+
+
